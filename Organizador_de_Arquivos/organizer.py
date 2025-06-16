12cb9fcd8f07e936d5f4b2f1b3c0b018b5e28b88
Davi Teixeira <124937763+txdavi@users.noreply.github.com>
2025-06-16 12:08:39 -0300

    Create organizer.py

---
+++ Organizador_de_Arquivos/organizer.py
@@ -0,0 +1,23 @@
+import os
+from tkinter.filedialog import askdirectory
+
+caminho = askdirectory(title="Selecione uma pasta")
+
+lista_arquivos = os.listdir(caminho)
+
+locais = {
+    "imagens": [".png", ".jpg"],
+    "planilhas": [".xlsx", ".csv"],
+    "pdfs": [".pdf"],
+    "zips": [".zip", ".rar"],
+    "programas": [".exe", ".msi"],
+    "anotações": [".txt", ".docx"]
+}
+
+for arquivo in lista_arquivos:
+    nome, extensao = os.path.splitext(f"{caminho}/{arquivo}")
+    for pasta in locais:
+        if extensao in locais[pasta]:
+            if not os.path.exists(f"{caminho}/{pasta}"):
+                os.mkdir(f"{caminho}/{pasta}")
+            os.rename(f"{caminho}/{arquivo}", f"{caminho}/{pasta}/{arquivo}")
